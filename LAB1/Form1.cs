eca5948f375b8eaaaf3ffe489dfe98da339b4b08
Полина Свиридова <polinasviri@gmail.com>
2026-02-18 22:33:03 +0300

    Add files via upload

---
+++ LAB1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+namespace LAB1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.button1.Click += new EventHandler(AnyButton_Click);
+            this.button2.Click += new EventHandler(AnyButton_Click);
+
+            this.button1.MouseLeave += new EventHandler(AnyButton_MouseLeave);
+            this.button2.MouseLeave += new EventHandler(AnyButton_MouseLeave);
+
+            this.button1.MouseEnter += new EventHandler(button1_MouseEnter);
+            this.button2.MouseEnter += new EventHandler(button2_MouseEnter);
+        }
+
+        private void AnyButton_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+        }
+
+        private void AnyButton_MouseLeave(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Text = "пришел";
+            button2.Text = "ушел";
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Text = "ушел";
+            button2.Text = "пришел";
+        }
+    }
+}
---
+++ LAB1/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB1_3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void AnyButton_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double number))
+            {
+                double result = number * 7;
+                textBox1.Text = result.ToString();
+            }
+            else
+            {
+                textBox1.Clear();
+            }
+        }
+    }
+}
